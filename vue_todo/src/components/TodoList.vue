4125495e82038d221e511ed926e1c60c68e35f0f
Greg Sánchez <gso@nextret.net>
2019-01-31 17:23:41 +0100

    Create TODO VUE example

---
+++ vue_todo/src/components/TodoList.vue
@@ -0,0 +1,23 @@
+<template>
+    <section>
+        <ul class="js-todo-list" v-for="task in tasks">
+            <li>{{task.text}}</li>
+        </ul>
+    </section>
+</template>
+<script>
+export default {
+  data () {
+    return {
+        /**
+         *  JSON  task -> {id : 1, text:'', status:''}
+         *  status -> 'completed', 'incompleted'
+         */
+        tasks: [
+            {id : 1, text:'T1', status:'C'},
+            {id : 2, text:'T2', status:'I'}
+        ]
+    }
+  }
+}
+</script>
